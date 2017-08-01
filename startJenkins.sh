e78c5774e6cb2b98f346222118f38e41580705b0
Lucie Hutchins <LucyHut@users.noreply.github.com>
2017-08-01 11:01:40 -0400

    Update startJenkins.sh

--- startJenkins.sh
+++ startJenkins.sh
@@ -2,7 +2,7 @@
 
 JENKINS_BASE=/opt/software/external/jenkins
 JENKINS_HOME=$JENKINS_BASE/.jenkins
-JAVA=/usr/bin/java
+JAVA=`which java`
 
 export JENKINS_HOME
 nohup $JAVA -jar $JENKINS_BASE/jenkins.war --httpPort=10080 -XX:MaxPermSize=512m &

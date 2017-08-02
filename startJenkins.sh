d33fcbc838a5623572417dfcc14ae383dbf55fca
LucyHut <lhutchin@lintilla.mdibl.net>
2017-08-02 14:00:31 -0400

    Removed trailing empty lines

--- startJenkins.sh
+++ startJenkins.sh
@@ -6,5 +6,3 @@ JAVA=`which java`
 
 export JENKINS_HOME
 nohup $JAVA -jar $JENKINS_BASE/jenkins.war --httpPort=10080 -XX:MaxPermSize=512m &
-~                                                                                                                  
-~        

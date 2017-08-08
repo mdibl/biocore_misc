688bee108e964cd3dd20e9debba34552fb763240
LucyHut <lucie.hutchins@yahoo.com>
2017-08-08 16:20:59 -0400

    Code refactoring

--- gitExportRepos.sh
+++ gitExportRepos.sh
@@ -27,18 +27,8 @@ TAR=`which tar`
 
 #setup the log file
 SCRIPT_NAME=`basename $0`
-WORKING_DIR=`dirname $0`
-LOG_DIR=$WORKING_DIR/logs
-
-if [ ! -d $LOG_DIR ]
-then
-   mkdir $LOG_DIR
-fi
-
-LOG=$LOG_DIR/$SCRIPT_NAME.log
-rm -f $LOG
-touch $LOG
-
+TOP=`dirname $0`
+WORKING_DIR=`realpath $TOP`
 #Check the number of arguments
 if [ $# -lt 4 ]
 then
@@ -63,10 +53,22 @@ INSTALL_DIR=$4
 GIT_URL=https://api.github.com/repos/$ORG/$REPO/tarball/$TAG
 #Local tag directory
 TAG_DIR=$REPO-$TAG
+LOG_DIR=$WORKING_DIR/logs
+
+if [ ! -d $LOG_DIR ]
+then
+   mkdir $LOG_DIR
+fi
+
+LOG=$LOG_DIR/$SCRIPT_NAME.$TAG_DIR.log
+rm -f $LOG
+touch $LOG
+
 #Results tar file
 TAG_TAR_FILE=$TAG_DIR.tar.gz
 
 date | tee -a $LOG
+echo "The path to logs is $LOG_DIR" | tee -a $LOG
 echo "wget path: $WGET" | tee -a $LOG
 echo "tar path: $TAR"| tee -a $LOG
 echo "Tag: $TAG"| tee -a $LOG
@@ -82,11 +84,15 @@ cd $INSTALL_DIR
 if [ -d $TAG_DIR ]
 then
    echo "The tag $TAG_DIR is already installed under $INSTALL_DIR"
+   if [ -d $TAG_DIR.old ]
+   then 
+      rm -rf $TAG_DIR.old
+   fi
    mv $TAG_DIR $TAG_DIR.old
 fi
 #execute the command
 echo Cammand: $WGET -O $TAG_TAR_FILE  "$GIT_URL" | tee -a $LOG
-$WGET -O $TAG_TAR_FILE  "$GIT_URL" | tee -a $LOG
+$WGET -O $TAG_TAR_FILE  "$GIT_URL"  2>&1 | tee -a $LOG
 
 #Create local directory for this tag
 mkdir $TAG_DIR

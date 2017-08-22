ab656116939dbc6f597bbe5d5c3a8fa29144addb
LucyHut <lucie.hutchins@yahoo.com>
2017-08-22 14:08:33 -0400

    Added python libs

---
+++ python/dao/dom/biocore_config.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+class Biocore:
+    def __init(self):
+        self.external_data_dir="/data/external"
+        self.internal_data_dir="/data/internal"
+
+        self.external_software_dir="/opt/software/external"
+        self.internal_software_dir="/opt/software/internal"
+   
+        self.log_dir="/data/logs"
+        self.data_downloads_log_dir="/data/logs/data_downloads"
+        
+        self.data_downloads_scripts_dir="/usr/local/biocore/data_downloads"
